2094e89c3d8f3b08df5284b52516c83a50ca833b
Bernese-Corgi <jnyng0621@gmail.com>
2021-09-24 04:15:23 +0900

    usePreloader custom Hook 생성 및 적용

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { User } from '../components';
-import { Preloader } from '../lib/PreloadContext';
+import { usePreloader } from '../lib/PreloadContext';
 import { getUser } from '../modules/users';
 
 const UserContainer = ({ id }) => {
   const user = useSelector(state => state.users.user);
   const dispatch = useDispatch();
+
+  usePreloader(() => dispatch(getUser(id))); // 서버 사이드 렌더링 할 때 API 호출하기
 
   useEffect(() => {
     // 중복 요청 방지 : 유저가 존재하고, id가 일치한다면 요청하지 않음
@@ -16,12 +18,10 @@ const UserContainer = ({ id }) => {
     dispatch(getUser(id));
   }, [dispatch, id, user]); // id가 바뀔 때 새로 요청해야 함
 
-  // 유효성 검사
-  // 아직 정보가 없는 경우 user 값이 null을 가리키므로, User 컴포넌트가 렌더링되지 않도록 컨테이너 컴포넌트에서 Preloader 컴포넌트를 반환한다.
+  /* 유효성 검사 --------------------------------- */
+  // 아직 정보가 없는 경우 user 값이 null을 가리키므로, User 컴포넌트가 렌더링되지 않도록 컨테이너 컴포넌트에서 null을 반환한다.
   // 서버 사이드 렌더링을 하는 과정에서 데이터가 없을 경우 GET_USER 액션을 발생시킨다.
-  if (!user) {
-    return <Preloader resolve={() => dispatch(getUser(id))} />;
-  }
+  if (!user) return null;
 
   return <User user={user} />;
 };
--- src/lib/PreloadContext.js
+++ src/lib/PreloadContext.js
@@ -20,3 +20,14 @@ export const Preloader = ({ resolve } /* reslove는 함수 타입 */) => {
 
   return null;
 };
+
+/* usePreloader 커스텀 훅 --------------------------- */
+export const usePreloader = resolve => {
+  const preloadContext = useContext(PreloadContext);
+  if (!preloadContext) return null;
+  if (preloadContext.done) return null;
+
+  // promises 배열에 프로미스 등록
+  // 설령 resolve 함수가 프로미스를 반환하지 않더라도, 프로미스 취급을 하기 위하여 Promise.resolve 함수 사용
+  preloadContext.promises.push(Promise.resolve(resolve()));
+};
